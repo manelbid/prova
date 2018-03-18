54af8d6ececd6bd08a90621ed75206b41442c03a
Manel <m151088@hotmail.com>
2018-03-18 13:59:10 +0100

    Merge origin/Branch1FB
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/prova1/Prova1.java
+++ src/prova1/Prova1.java
@@ -16,7 +16,7 @@ public class Prova1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("prova2");
+        System.out.println("prova");
     }
     
 }
